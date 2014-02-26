254d4c325d9ae6de26fb25037be7b27b82ab616a
Roman Bolshakov <rombolshak@gmail.com>
2014-02-26 22:53:32 +0400

    Additional task params

--- task.cpp
+++ task.cpp
@@ -4,13 +4,23 @@ Task::Task(double alpha,
 	   double (*f)(double, double), 
 	   double (*initial)(double),
 	   double (*boundaryLeft)(double),
-	   double (*boundaryRight)(double))
+	   double (*boundaryRight)(double),
+	   double maxTime,
+	   double minCoord,
+	   double maxCoord,
+	   double timeStep,
+	   double coordStep)
 {
     _alpha = alpha;
     _f = f;
     _initial = initial;
     _boundaryLeft = boundaryLeft;
     _boundaryRight = boundaryRight;
+    _maxTime = maxTime;
+    _minCoord = minCoord;
+    _maxCoord = maxCoord;
+    _timeStep = timeStep;
+    _coordStep = coordStep;
 }
 
 double Task::getAlpha()
@@ -36,4 +46,29 @@ double Task::calcLeftBoundary(double t)
 double Task::calcRightBoundary(double t)
 {
     return _boundaryRight(t);
+}
+
+double Task::getMaxTime()
+{
+    return _maxTime;
+}
+
+double Task::getMinCoord()
+{
+    return _minCoord;
+}
+
+double Task::getMaxCoord()
+{
+    return _maxCoord;
+}
+
+double Task::getTimeStep()
+{
+    return _timeStep;
+}
+
+double Task::getCoordStep()
+{
+    return _coordStep;
 }
--- task.h
+++ task.h
@@ -9,13 +9,24 @@ public:
 	 double (*f)(double, double), 
 	 double (*initial)(double),
 	 double (*boundaryLeft)(double),
-	 double (*boundaryRight)(double));
+	 double (*boundaryRight)(double),
+	 double maxTime = 10,
+	 double minCoord = 0,
+	 double maxCoord = 1,
+	 double timeStep = 0.1,
+	 double coordStep = 0.01
+	);
     
     double getAlpha();
     double calcF(double x, double t);
     double calcInitial(double x);
     double calcLeftBoundary(double t);
     double calcRightBoundary(double t);
+    double getMaxTime();
+    double getMinCoord();
+    double getMaxCoord();
+    double getTimeStep();
+    double getCoordStep();
     
 private:
     double _alpha;
@@ -23,6 +34,11 @@ private:
     double (*_boundaryLeft)(double);
     double (*_initial)(double);
     double (*_f)(double, double);
+    double _maxTime;
+    double _minCoord;
+    double _maxCoord;
+    double _timeStep;
+    double _coordStep;
 };
 
 #endif // TASK_H
