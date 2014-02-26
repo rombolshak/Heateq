ae80937475ae2c4861cb75c1031c28f784e3ae66
Roman Bolshakov <rombolshak@gmail.com>
2014-02-26 21:16:07 +0400

    Equation condition functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+/*
+ * In order to solve another heat equation
+ * you need to change 4 functions below
+ * and recompile the program.
+ * 
+ * Create a math expression parser is huge overkill imho
+ */
+
+double f(double x, double t) {
+    return 0;
+}
+
+double initial(double x) {
+    return 0;
+}
+
+double boundaryLeft(double t) {
+    return 0;
+}
+
+double boundaryRight(double t) {
+    return 0;
+}
+
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
     return 0;
